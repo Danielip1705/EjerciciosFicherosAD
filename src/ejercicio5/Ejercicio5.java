57f8ba13c828b5ec5b5cefff4363f2d8f1b44ed7
diglesias <diglesias@107-05>
2024-10-30 11:13:04 +0100

    Realizar Ejercicio 5

---
+++ src/ejercicio5/Ejercicio5.java
@@ -0,0 +1,23 @@
+package ejercicio5;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		
+		try {
+			RandomAccessFile lectura = new RandomAccessFile("C:\\Users\\diglesias\\eclipse-workspace\\EjercicioFicheros\\src", "rw");
+			
+			
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
